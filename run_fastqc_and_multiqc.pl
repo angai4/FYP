e7375eb5d51330e93adeed4cfa349e18512cb8a4
Aaron Ngai <156691852+angai4@users.noreply.github.com>
2024-02-06 17:21:32 +0000

    Update run_fastqc_and_multiqc.pl

--- run_fastqc_and_multiqc.pl
+++ run_fastqc_and_multiqc.pl
@@ -3,14 +3,12 @@
 use strict;
 use warnings;
 
-# Define the directory where the raw fastq files are
+# Define the directory you want to change to
 my $directory = "/media/newdrive/GCB2024/aaronngai/automation/raw_fastq";
-
-# Define the directory where you want the output of FastQC to go
 my $fastqc_output_dir = "/media/newdrive/GCB2024/aaronngai/automation/raw_fastq/FastQ\
 C";
 
-# Change to the directory with the raw fastq files
+# Change to the specified directory
 chdir $directory or die "Cannot change to directory $directory: $!";
 
 print "Successfully changed to directory: $directory\n";
@@ -21,4 +19,15 @@ my @fastq_files = glob("*.fastq.gz"); # maybe use grep
 foreach my $file (@fastq_files) {
     # Run FastQC
     my $cmd = "fastqc --outdir=$fastqc_output_dir $file";
+        print "Running: $cmd\n";
+    system($cmd) == 0 or warn "Failed to run $cmd: $!";
+}
 
+print "FastQC analysis complete.\n";
+
+# Run MultiQC on the FastQC output
+
+my $multiqc_cmd = "multiqc -o $fastqc_output_dir $fastqc_output_dir";
+system($multiqc_cmd) == 0 or die "Failed to run MultiQC: $!";
+
+print "MultiQC report generated.\n";
