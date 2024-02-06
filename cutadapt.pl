e672ed458abd7f470fff6d98a1bedeec50f870c3
Aaron Ngai <156691852+angai4@users.noreply.github.com>
2024-02-06 19:08:25 +0000

    Update cutadapt.pl

--- cutadapt.pl
+++ cutadapt.pl
@@ -1,6 +1,12 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use File::Path 'make_path';
+
+my $dir = "$ENV{HOME}/automation/trimmed_fastq/FastQC";
+
+# The make_path function will create the directory and any necessary parent directories
+make_path($dir);
 
 # Define the directory you want to change to
 my $directory = "/media/newdrive/GCB2024/aaronngai/automation/raw_fastq";
@@ -14,5 +20,38 @@ print "Successfully changed to directory: $directory\n";
 my @files = glob("*_1.fastq.gz");
 
 foreach my $file (@files) {
-    # Extracting the base name for the output file using substitution regular expression
-    (my $base = $file) =~ s/_1\.fastq\.gz//; # substitute the pattern found in between with nothing"/ /" 
+    # Extracting the base name for the output file using substitution regular express\
+ion
+    (my $base = $file) =~ s/_1\.fastq\.gz//; # substitute the pattern found in betwee\
+n "/ / with nothing"
+
+    # Constructing the cutadapt command
+
+    my $cutadapt = "cutadapt -j=1 --output=trimmed_${base}_1.fastq.gz ".
+              "--paired-output=trimmed_${base}_2.fastq.gz ".
+              "--error-rate=0.1 --times=1 --overlap=3 ".
+              "--action=trim --minimum-length=20 --pair-filter=any ".
+              "--quality-cutoff=20 ${base}_1.fastq.gz ${base}_2.fastq.gz ".
+        "> trimmed_${base}_cutadapt.log"; # "" and . for better readibility instead o\
+f one long string
+
+    # Execute the command
+    system($cutadapt) == 0
+        or die "Failed to execute cutadapt: $!";
+}
+
+# move trimmed data - open the source directory
+opendir(my $dh, $directory) or die "Cannot open directory $directory: $!";
+
+while (my $file = readdir($dh)) {
+    # If the file starts with "trimmed", move it to the target directory
+    if ($file =~ /^trimmed/) {
+        my $source_path = "$directory/$file";
+        my $target_path = "$dir/$file";
+        rename($source_path, $target_path) or die "Cannot move $source_path to $targe\
+t_path: $!";
+    }
+}
+
+closedir($dh);
+
