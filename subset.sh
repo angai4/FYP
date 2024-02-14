5dc6134f95dde77487fff06e9831f49ef2846b84
Aaron Ngai <156691852+angai4@users.noreply.github.com>
2024-02-14 14:58:05 +0000

    Update subset.sh

--- subset.sh
+++ subset.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This code will extract a subset of reads from gzipped fastq files and output them to new subset gzipped fastq files to allow for faster script development
+# This code will extract a subset of reads from gzipped fastq files and output them to new subset gzipped fastq files and remove the original files
+# Working with subset FASTQ files will allow for faster script testing/development
 
 # Number of reads to extract
 num_reads=100  # Number of reads you want to extract
@@ -19,6 +20,10 @@ for file in *.fastq.gz; do
     zcat "$file" | head -n $lines_to_extract | gzip > "$output_name"
    echo "Extracted $num_reads reads from\
  $file to $output_name"
+
+ # Remove the original gzipped FASTQ file
+   rm $file
+   echo "Removed original file: $file"
 done
 
 
