d7f4bda5aec9f9a522dcbc825710ae2d1079deb0
Aaron Ngai <156691852+angai4@users.noreply.github.com>
2024-02-28 12:28:20 +0000

    Update indexbam.pl

--- indexbam.pl
+++ indexbam.pl
@@ -2,21 +2,27 @@
 use strict;
 use warnings;
 
-# This script will index bam files within the current directory
+# This script will change to the directory that contains the bams and index the bam files
 
 # -@ specify number of threads to use
 for i in *Aligned.sortedByCoord.out.bam; do
 samtools index $i -@ 12
 done
 
-# Getting all the files matching the pattern *_1.fastq.gz
-my @files = glob("*_1.fastq.gz");
+# Specify the directory that contains bam files
+my bam_path = 
 
-foreach my $file (@files) {
-    # Extracting the base name for the output file using substitution regular express\
-ion
-    (my $base = $file) =~ s/_1\.fastq\.gz//; # substitute the pattern found in betwee\
-n "/ / with nothing"
+# Change to directory with the bam files
+
+chdir $bam_path or die "Cannot change to directory $bam_path: $!";
+print "Successfully changed to directory: $bam_path\n";
+
+# Getting all the files matching the pattern *
+my @bamfiles = glob("*");
+
+foreach my $file (@bamfiles) {
+    # Extracting the base name for the output file using substitution regular expression
+    (my $base = $file) =~ s/_1\.fastq\.gz//; # substitute the pattern found in between "/ / with nothing"
 
     # Constructing the cutadapt command
 
@@ -25,8 +31,7 @@ n "/ / with nothing"
              "--error-rate=0.1 --times=1 --overlap=3 ".
               "--action=trim --minimum-length=20 --pair-filter=any ".
               "--quality-cutoff=20 ${base}_1.fastq.gz ${base}_2.fastq.gz ".
-        "> trimmed_${base}_cutadapt.log"; # "" and . for better readibility instead o\
-f one long string
+        "> trimmed_${base}_cutadapt.log"; # "" and . for better readibility instead of one long string
 
     # Execute the command
     system($cutadapt) == 0
