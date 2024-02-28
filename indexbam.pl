fe03b8f2952f373fee05b6c3506f044921918dad
Aaron Ngai <156691852+angai4@users.noreply.github.com>
2024-02-28 12:42:23 +0000

    Update indexbam.pl

--- indexbam.pl
+++ indexbam.pl
@@ -4,11 +4,6 @@ use warnings;
 
 # This script will change to the directory that contains the bams and index the bam files
 
-# -@ specify number of threads to use
-for i in *Aligned.sortedByCoord.out.bam; do
-samtools index $i -@ 12
-done
-
 # Check for the correct number of arguments
 if (@ARGV < 1) {
     die "Usage: $0 <path_to_bam_dir>\n";
@@ -27,24 +22,22 @@ unless (-x $bam_path) {
 chdir $bam_path or die "Cannot change to directory $bam_path: $!";
 print "Successfully changed to directory: $bam_path\n";
 
-# Getting all the files matching the pattern *
-my @bamfiles = glob("*");
+# Getting all the files matching the pattern *Aligned.sortedByCoord.out.bam
+my @bamfiles = glob("*Aligned.sortedByCoord.out.bam");
 
 foreach my $file (@bamfiles) {
     # Extracting the base name for the output file using substitution regular expression
-    (my $base = $file) =~ s/_1\.fastq\.gz//; # substitute the pattern found in between "/ / with nothing"
+    (my $base = $file) =~ s/*Aligned\.sortedByCoord\.out\.bam//; # substitute the pattern found in between "/ / with nothing"
 
-    # Constructing the cutadapt command
+    # Constructing the index command
+    # -@ specify number of threads to use
 
-    my $cutadapt = "cutadapt -j=1 --output=trimmed_${base}_1.fastq.gz ".
-              "--paired-output=trimmed_${base}_2.fastq.gz ".
-             "--error-rate=0.1 --times=1 --overlap=3 ".
-              "--action=trim --minimum-length=20 --pair-filter=any ".
-              "--quality-cutoff=20 ${base}_1.fastq.gz ${base}_2.fastq.gz ".
-        "> trimmed_${base}_cutadapt.log"; # "" and . for better readibility instead of one long string
+    my $indexbam = "for i in *Aligned.sortedByCoord.out.bam; do
+samtools index $i -@ 12
+done"; 
 
     # Execute the command
-    system($cutadapt) == 0
-        or die "Failed to execute cutadapt: $!";
+    system($indexbam) == 0
+        or die "Failed to execute indexbam: $!";
 }
 
