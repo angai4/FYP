85c9689d888f78562897fba51d41979fda4e3728
Aaron Ngai <156691852+angai4@users.noreply.github.com>
2024-02-28 12:23:14 +0000

    Update indexbam.pl

--- indexbam.pl
+++ indexbam.pl
@@ -2,4 +2,11 @@
 use strict;
 use warnings;
 
-# This script will 
+# This script will index bam files within the current directory
+
+# -@ specify number of threads to use
+for i in *Aligned.sortedByCoord.out.bam; do
+samtools index $i -@ 12
+done
+
+foreach 
